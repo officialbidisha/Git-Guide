31e548996c3934a00f63cadf35b4f3ee81532337
Bidisha Das <officialbidisha@gmail.com>
2021-12-06 03:10:08 +0530

    Accessible http.

--- src/app/gitservices.service.ts
+++ src/app/gitservices.service.ts
@@ -9,39 +9,29 @@ export class GitservicesService {
   public username: string;
   public clientId = 'ed1c379ecbdfb4f7cf88';
   public clientSecret = 'f7ff42188f369a3aa48cfa3601817f8144585efb';
-  public headerDict = {
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
-    'Access-Control-Allow-Headers': 'Content-Type',
-  };
 
-  public requestOptions = {
-    headers: new HttpHeaders(this.headerDict),
-  };
   constructor(private http: HttpClient) {
     console.log('Service is now ready');
     this.username = 'officialbidisha';
   }
   public getProfileDetails(): Observable<any> {
     return this.http.get(
-      'https://api.github.com/users/' +
+      'http://api.github.com/users/' +
         this.username +
         '?client_id=' +
         this.clientId +
         '&client_secret=' +
-        this.clientSecret,
-        this.requestOptions
+        this.clientSecret
     );
   }
   public getRepoDetails(): Observable<any> {
     return this.http.get(
-      'https://api.github.com/users/' +
+      'http://api.github.com/users/' +
         this.username +
         '/repos?client_id=' +
         this.clientId +
         'client_secret=' +
-        this.clientSecret,
-        this.requestOptions
+        this.clientSecret
     );
   }
   public updateProfile(username: string): void {
@@ -49,12 +39,11 @@ export class GitservicesService {
   }
   public getWeekelyDetails(reponame): Observable<any> {
     return this.http.get(
-      'https://api.github.com/repos/' +
+      'http://api.github.com/repos/' +
         this.username +
         '/' +
         reponame +
-        '/stats/contributors',
-        this.requestOptions
+        '/stats/contributors'
     );
   }
 }
